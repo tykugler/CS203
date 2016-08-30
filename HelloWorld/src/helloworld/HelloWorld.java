bc766b447d4ffd08b431a565fc49902f0c399b77
Tyler Kugler <tykugler@vassar.edu>
2016-08-30 15:47:47 -0400

    First snapshot

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author tykugler
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     * @return the statement "Hello, World."
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello, World.");
+    }
+    
+}
