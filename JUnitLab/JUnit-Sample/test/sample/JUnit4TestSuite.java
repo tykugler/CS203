1b39adedaf91d70af3d1dd5eb7fba668c5e06d73
Tyler Kugler <tykugler@vassar.edu>
2016-09-13 15:43:45 -0400

    First snapshot

---
+++ JUnitLab/JUnit-Sample/test/sample/JUnit4TestSuite.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author tykugler
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({sample.UtilsJUnit4Test.class, sample.VectorsJUnit4Test.class})
+public class JUnit4TestSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+}
